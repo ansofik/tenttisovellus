9c538db61f4a064a0e1696532df549d382ac9a64
sofia <84174004+sofiak0@users.noreply.github.com>
2022-10-19 09:03:22 +0300

    modified reducer

--- src/App.js
+++ src/App.js
@@ -29,29 +29,81 @@ const tenttiLista = [tentti1, tentti2];
 
 function reducer(state, action) {
 
-  const tentinIndex = action.payload.tentinIndex;
-  const tentitKopio = [...state];
+  /* const tentinIndex = action.payload.tentinIndex;
+  const tentitKopio = [...state]; */
 
   switch (action.type) {
-    case 'TENTIN_NIMI_MUUTTUI':
-      tentitKopio[tentinIndex].nimi = action.payload.nimi;
-      return tentitKopio;
+    case 'TENTIN_NIMI_MUUTTUI': {
+      /* tentitKopio[tentinIndex].nimi = action.payload.nimi;
+      return tentitKopio; */
 
-    case 'KYSYMYS_MUUTTUI':
-      tentitKopio[tentinIndex].kysymykset[action.payload.kysymyksenIndex].kysymys = action.payload.kysymys;
-      return tentitKopio;
+      // muuttamatta tilaa suoraan:
+      /* const tenttiKopio = {...tentitKopio[tentinIndex]};
+      tenttiKopio.nimi = action.payload.nimi;
+      tentitKopio[tentinIndex] = tenttiKopio;
+      return tentitKopio; */
 
-    case 'POISTA_KYSYMYS':
-      let tenttiKopio = { ...tentitKopio[action.payload.tentinIndex] };
-      let kysymyksetKopio = [...tenttiKopio.kysymykset];
+      // lyhyemmin:
+      return state.map((tentti, i) => (i == action.payload.tentinIndex ? { ...tentti, nimi: action.payload.nimi } : tentti));
+
+    }
+
+    case 'KYSYMYS_MUUTTUI': {
+      /* tentitKopio[tentinIndex].kysymykset[action.payload.kysymyksenIndex].kysymys = action.payload.kysymys;
+      return tentitKopio; */
+
+      /* const kysymyksenIndex = action.payload.kysymyksenIndex;
+      const tenttiKopio = { ...tentitKopio[tentinIndex] };
+      const kysymyksetKopio = [...tenttiKopio.kysymykset];
+      const kysymysKopio = { ...kysymyksetKopio[kysymyksenIndex] };
+      kysymysKopio.kysymys = action.payload.kysymys;
+
+      kysymyksetKopio[kysymyksenIndex] = kysymysKopio;
+      tenttiKopio.kysymykset = kysymyksetKopio;
+      tentitKopio[tentinIndex] = tenttiKopio;
+      return tentitKopio; */
+
+      return state.map((tentti, i) => (
+        i == action.payload.tentinIndex ? {
+          ...tentti, kysymykset: tentti.kysymykset.map((kysymys, i) => (
+            i === action.payload.kysymyksenIndex ? { ...kysymys, kysymys: action.payload.kysymys } : kysymys))
+        } : tentti));
+    }
+
+    case 'POISTA_KYSYMYS': {
+      /* const tenttiKopio = { ...tentitKopio[action.payload.tentinIndex] };
+      const kysymyksetKopio = [...tenttiKopio.kysymykset];
       kysymyksetKopio.splice(action.payload.kysymyksenIndex, 1);
       tenttiKopio.kysymykset = kysymyksetKopio;
       tentitKopio[tentinIndex] = tenttiKopio;
-      return tentitKopio;
+      return tentitKopio; */
 
-    case 'VASTAUS_MUUTTUI':
-      tentitKopio[tentinIndex].kysymykset[action.payload.kysymyksenIndex].vastausVaiht[action.payload.vastauksenIndex] = action.payload.vastaus;
+      return state.map((tentti, i) => (
+        i == action.payload.tentinIndex ? {
+          ...tentti, kysymykset: tentti.kysymykset.filter((kysymys, j) => j != action.payload.kysymyksenIndex)
+        } : tentti));
+    }
+
+    case 'VASTAUS_MUUTTUI': {
+      /* tentitKopio[tentinIndex].kysymykset[action.payload.kysymyksenIndex].vastausVaiht[action.payload.vastauksenIndex] = action.payload.vastaus;
+      return tentitKopio; */
+      const tentitKopio = [...state];
+      const tentinIndex = action.payload.tentinIndex;
+      const kysymyksenIndex = action.payload.kysymyksenIndex;
+      const vastauksenIndex = action.payload.vastauksenIndex;
+      const tenttiKopio = { ...tentitKopio[tentinIndex] };
+      const kysymyksetKopio = [...tenttiKopio.kysymykset];
+      const kysymysKopio = { ...kysymyksetKopio[kysymyksenIndex] };
+      const vastausVaihtKopio = [...kysymysKopio.vastausVaiht];
+      vastausVaihtKopio[vastauksenIndex] = action.payload.vastaus;
+
+      kysymysKopio.vastausVaiht = vastausVaihtKopio;
+      kysymyksetKopio[kysymyksenIndex] = kysymysKopio;
+      tenttiKopio.kysymykset = kysymyksetKopio;
+      tentitKopio[tentinIndex] = tenttiKopio;
+
       return tentitKopio;
+    }
 
     default:
       throw new Error();
