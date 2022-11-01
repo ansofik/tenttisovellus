af0043db3e1f2474fcb6951040fe3605bf41cd3c
sofia <84174004+sofiak0@users.noreply.github.com>
2022-11-01 20:31:19 +0200

    query functions

--- server/dbtest.js
+++ server/dbtest.js
@@ -1,3 +1,4 @@
+const { query } = require('express')
 const { Pool, Client } = require('pg')
 const pool = new Pool({
   user: 'postgres',
@@ -7,10 +8,13 @@ const pool = new Pool({
   port: 5432,
 })
 
-/* lisää uusi tentti */
-const lisaaTentti = async () => {
+// lisää uusi tentti
+const lisaaTentti = async (nimi, pvm, voimassa) => {
   try {
-    const query = "INSERT INTO tentit (nimi, pvm) VALUES ('Python perusteet', '2020-02-02'), ('C perusteet', '2021-01-01')"
+    const query = {
+      text: 'INSERT INTO tentit (nimi, pvm, voimassa) VALUES ($1, $2, $3)',
+      values: [nimi, pvm, voimassa]
+    }
     const res = await pool.query(query)
     console.log("added rows", res.rowCount)
   } catch (err) {
@@ -18,27 +22,24 @@ const lisaaTentti = async () => {
   }
 }
 
-/* lisaaTentti() */
+//lisaaTentti('Python perusteet', '2020-02-02', false)
 
 
-/* poista tentti id:n perusteella */
+// poista tentti id:n perusteella
 const poistaTentti = async (id) => {
   try {
-    const query = {
-      text: 'DELETE FROM tentit WHERE id = $1',
-      values: [id]
-    }
-    const res = await pool.query(query)
+    const queryText = 'DELETE FROM tentit WHERE id = $1'
+    const res = await pool.query(queryText, [id])
     console.log(res.rowCount)
   } catch (err) {
-    console.log("virhe tentin poistamisessa", err)
+    console.log("virhe tentin poistamisessa indeksin perusteella", err)
   }
 }
 
-/* poistaTentti(5) */
+//poistaTentti(5)
 
 
-/* muuta tentin nimeä */
+// muuta tentin nimeä
 const muutaTentinNimea = async (id, uusiNimi) => {
   try {
     const query = {
@@ -52,10 +53,10 @@ const muutaTentinNimea = async (id, uusiNimi) => {
   }
 }
 
-/* muutaTentinNimea(2, 'SQL perusteet') */
+//muutaTentinNimea(2, 'SQL perusteet')
 
 
-/* hae kaikki tentit */
+// hae kaikki tentit
 const haeTentit = async () => {
   try {
     const res = await pool.query('SELECT * FROM tentit')
@@ -65,27 +66,23 @@ const haeTentit = async () => {
   }
 }
 
-/* haeTentit() */
+//haeTentit()
 
-
-/* hae tentti id:n perusteella */
+// hae tentti id:n perusteella
 const haeTentti = async (id) => {
   try {
-    const query = {
-      text: 'SELECT * FROM tentit WHERE id = $1',
-      values: [id]
-    }
-    const res = await pool.query(query)
+    const queryText = 'SELECT * FROM tentit WHERE id = $1'
+    const res = await pool.query(queryText, [id])
     console.log(res.rows)
   } catch (err) {
     console.log("virhe tentin hakemisessa", err)
   }
 }
 
-/* haeTentti(1) */
+//haeTentti(1)
 
 
-/* hae tentit nimen perusteella aakkosjärjestyksessä */
+// hae tentit nimen perusteella aakkosjärjestyksessä 
 const haeTentitAakkosj = async () => {
   try {
     const res = await pool.query('SELECT * FROM tentit ORDER BY nimi')
@@ -95,5 +92,52 @@ const haeTentitAakkosj = async () => {
   }
 }
 
-/* haeTentitAakkosj() */
+//haeTentitAakkosj()
 
+
+// Hae tentit, joiden indeksi on listalla
+const haeTentitId = async (idList) => {
+  try {
+    let str = ''
+    for (let i = 1; i <= idList.length; i++) {
+      str += ' $' + i + ','
+    }
+    str = str.trim().slice(0, -1)
+    const queryText = 'SELECT * FROM tentit WHERE id IN (' + str + ')'
+    console.log(queryText)
+    const res = await pool.query(queryText, idList)
+    console.log(res.rows)
+  } catch (err) {
+    console.log("virhe tenttien hakemisessa", err)
+  }
+}
+
+//haeTentitId([1,2,3])
+
+
+// Hae tentit, joiden päivämäärä on ennen annettua päivämäärää
+const haeTentitEnnenPvm = async (pvm) => {
+  try {
+     const queryText = 'SELECT * FROM tentit WHERE pvm < $1'
+     const res = await pool.query(queryText, [pvm])
+     console.log(res.rows)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+//haeTentitEnnenPvm('2022-10-12')
+
+
+// Hae tentit, jotka ovat voimassa 
+const haeTentitVoimassa = async () => {
+  try {
+    const queryText = 'SELECT * FROM tentit WHERE voimassa = true'
+    const res = await pool.query(queryText)
+    console.log(res.rows)
+  } catch (err) {
+    console.log("virhe tenttien hakemisessa", err)
+  }
+}
+
+//haeTentitVoimassa()
