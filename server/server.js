065e3d45f188f74010d3d5d696a75942ecc6b3bb
sofia <84174004+sofiak0@users.noreply.github.com>
2022-11-09 01:03:34 +0200

    add get for a taken exam

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const app = express()
 const port = 8080
 const examsRouter = require('./controllers/exams')
+const takenExamsRouter = require('./controllers/takenExam')
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/exams', examsRouter)
+app.use('/takenexams', takenExamsRouter)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
