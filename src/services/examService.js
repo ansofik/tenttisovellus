58f177c098b0496e7a4a821428f70ba00bcc3676
sofia <84174004+sofiak0@users.noreply.github.com>
2022-12-13 12:08:51 +0200

    fix undefined token error

--- src/services/examService.js
+++ src/services/examService.js
@@ -1,87 +1,91 @@
 import axios from 'axios'
 
 const url = 'http://localhost:8080'
-const config = {
-  headers: {
-    Authorization: `bearer ${JSON.parse(localStorage.getItem('loggedInUser')).token}`
+
+
+const getConfig = () => {
+  return {
+    headers: {
+      Authorization: `bearer ${JSON.parse(localStorage.getItem('loggedInUser')).token}`
+    }
   }
 }
 
 const getExams = async () => {
   console.log('get exam titles from server')
-  const response = await axios.get(`${url}/exams`, config)
+  const response = await axios.get(`${url}/exams`, getConfig())
   console.log('response', response);
   return response.data
 }
 
 const getPublishedExams = async () => {
-  const response = await axios.get(`${url}/userexams`, config)
+  const response = await axios.get(`${url}/userexams`, getConfig())
   console.log('response', response);
   return response.data
 }
 
 const getExam = async (id) => {
-  const response = await axios.get(`${url}/exams/${id}`, config)
+  const response = await axios.get(`${url}/exams/${id}`, getConfig())
   console.log('response', response);
   return response.data
 }
 
 const getUserExam = async (id) => {
-  const response = await axios.get(`${url}/userexams/${id}`, config)
+  const response = await axios.get(`${url}/userexams/${id}`, getConfig())
   console.log('response', response);
   return response.data
 }
 
 const takeExam = async (examId) => {
-  const response = await axios.post(`${url}/takenexams`, { examId: examId}, config)
+  const response = await axios.post(`${url}/takenexams`, { examId: examId }, getConfig())
   console.log('response', response);
   return response.data
 }
 
-const saveSelectedOption = async (takenExamId, optionId) => {
-  const response = await axios.post(`${url}/takenexams/${takenExamId}/answers`, {answerOptionId: optionId}, config)
+const saveSelectedOption = async (takenExamId, questionId, optionId) => {
+  const response = await axios.post(`${url}/takenexams/${takenExamId}/answers`, { questionId: questionId, optionId: optionId }, getConfig())
   console.log('response', response);
   return response.data
 }
 
-const returnExam = async (takenExamId) => {
+const returnExam = async (takenExamId, selectedOptionIds) => {
   console.log('returning exam');
-  const response = await axios.put(`${url}/takenexams/${takenExamId}`, {}, config)
+  const response = await axios.put(`${url}/takenexams/${takenExamId}`, { selectedOptionIds: selectedOptionIds }, getConfig())
 }
 
 const addExam = async () => {
-  const response = await axios.post(`${url}/exams`, { name: 'Uusi tentti'}, config)
+  const response = await axios.post(`${url}/exams`, { name: 'Uusi tentti' }, getConfig())
   console.log('response', response);
   return response.data
 }
 
-const deleteExam = (id) => axios.delete(`${url}/exams/${id}/`, config)
+const deleteExam = (id) => axios.delete(`${url}/exams/${id}/`, getConfig())
 
-const updateExamName = (id, name) => axios.put(`${url}/exams/${id}/`, { name: name }, config)
+const updateExamName = (id, name) => axios.put(`${url}/exams/${id}/`, { name: name }, getConfig())
 
 const addQuestion = async (id) => {
-  const response = await axios.post(`${url}/exams/${id}/questions`, { text: '' }, config)
+  const response = await axios.post(`${url}/exams/${id}/questions`, { text: '' }, getConfig())
   console.log('response', response);
   return response.data
 }
 
-const deleteQuestion = (id) => axios.delete(`${url}/questions/${id}/`, config)
+const deleteQuestion = (id) => axios.delete(`${url}/questions/${id}/`, getConfig())
 
-const updateQuestion = (id, text) => axios.put(`${url}/questions/${id}/`, { questionText: text }, config)
+const updateQuestion = (id, text) => axios.put(`${url}/questions/${id}/`, { questionText: text }, getConfig())
 
 const addOption = async (id) => {
-  const response = await axios.post(`${url}/questions/${id}/options`, { text: '', correct: false }, config)
+  const response = await axios.post(`${url}/questions/${id}/options`, { text: '', correct: false }, getConfig())
   console.log('response', response);
   return response.data
 }
 
-const deleteOption = (id) => axios.delete(`${url}/options/${id}/`, config)
+const deleteOption = (id) => axios.delete(`${url}/options/${id}/`, getConfig())
 
 const updateOption = (id, text, correct) => {
   console.log('updating option');
   console.log('correct', correct)
-  
-  axios.put(`${url}/options/${id}/`, { text: text, correct: correct }, config)
+
+  axios.put(`${url}/options/${id}/`, { text: text, correct: correct }, getConfig())
 }
 
 const examService = {
